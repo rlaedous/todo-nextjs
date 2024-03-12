dd212b50fddde4f07f8fb5ed35f9404186a5d25a
rlaeodus <gwg03185@gmail.com>
2024-03-12 11:48:12 +0900

    Feat : 1-2 + tanstack query devtools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import QueryProvider from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="flex gap-4 p-4 bg-blue-500 text-white">
-          <Link href="/about">about</Link>
-          <Link href="/report">report</Link>
-          <Link href="/todos-csr">todos-csr</Link>
-          <Link href="/todos-ssr">todos-ssr</Link>
-        </nav>
+        <QueryProvider>
+          <nav className="flex gap-4 p-4 bg-blue-500 text-white">
+            <Link href="/about">about</Link>
+            <Link href="/report">report</Link>
+            <Link href="/todos-csr">todos-csr</Link>
+            <Link href="/todos-ssr">todos-ssr</Link>
+          </nav>
+        </QueryProvider>
         {children}
       </body>
     </html>
