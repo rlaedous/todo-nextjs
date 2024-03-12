5ff6872496720a1b641f7b9dfa71e3c55dba79e3
rlaeodus <gwg03185@gmail.com>
2024-03-12 18:01:52 +0900

    Feat : 5-2 (todo ISR)

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -11,6 +11,12 @@ const ReportPage = async () => {
 
   return (
     <>
+      <div>{`현재까지 ${results.length}개의 todolist가 등록되었습니다.`}</div>
+      <div>{`현재 까지 ${
+        results.filter((result: Todo) => result.isDone === true).length
+      }개의 완료 리스트, ${
+        results.filter((result: Todo) => result.isDone === false).length
+      }개의 할일 리스트가 존재합니다.`}</div>
       {results.map((result: Todo) => (
         <div key={result.id}>
           <div>id: {result.id}</div>
