49bd9758f8b4fa0c746f3ce6af342e85281ca63b
rlaeodus <gwg03185@gmail.com>
2024-03-12 17:47:02 +0900

    Comment : SSG - 캐시 비우기 및 강력새로고침

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,10 @@ import React from "react";
 const AboutPage = async () => {
   const response = await fetch(`http://localhost:4000/companyInfo`);
   const results = await response.json();
-  console.log("results", results);
 
   return (
     <>
       <div>{results.name}</div>
-      <div>dd1d</div>
       <div>{results.description}</div>
     </>
   );
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,7 +1,26 @@
+import { Todo } from "@/types/types";
 import React from "react";
 
-const ReportPage = () => {
-  return <div>ReportPage</div>;
+const ReportPage = async () => {
+  const response = await fetch(`http://localhost:4000/todos`, {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const results = await response.json();
+
+  return (
+    <>
+      {results.map((result: Todo) => (
+        <div key={result.id}>
+          <div>id: {result.id}</div>
+          <div>title: {result.title}</div>
+          <div>contents: {result.contents}</div>
+          <div>isDone: {result.isDone.toString()}</div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ReportPage;
